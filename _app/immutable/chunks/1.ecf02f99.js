366d9b2f3a7d15bf6175a04c568609873d230ec1
LorisSigrist <43482866+LorisSigrist@users.noreply.github.com>
2023-04-01 12:33:36 +0000

    Deploying to gh-pages from @ LorisSigrist/sveltekit-static-sw-not-working-over-https@bbb00e42ff16b7aaa0c48a8d753d698b3fe725ac 🚀

--- _app/immutable/chunks/1.ecf02f99.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3a621493.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
-  e + "/_app/immutable/entry/app.d45cacc6.js",
+  e + "/_app/immutable/entry/app.331669c8.js",
   e + "/_app/immutable/chunks/0.0d1092c2.js",
-  e + "/_app/immutable/chunks/1.ecf02f99.js",
+  e + "/_app/immutable/chunks/1.4fc1b5b0.js",
   e + "/_app/immutable/chunks/2.09f92aaf.js",
   e + "/_app/immutable/chunks/_layout.da46b06b.js",
   e + "/_app/immutable/chunks/index.33fbba09.js",
-  e + "/_app/immutable/chunks/singletons.88d8fc19.js",
-  e + "/_app/immutable/entry/start.8319ca69.js",
-  e + "/_app/immutable/entry/error.svelte.3a621493.js",
+  e + "/_app/immutable/chunks/singletons.dfedfa85.js",
+  e + "/_app/immutable/entry/start.806c31f2.js",
+  e + "/_app/immutable/entry/error.svelte.8b56ea95.js",
   e + "/_app/immutable/entry/layout.svelte.b56ece7e.js",
   e + "/_app/immutable/entry/_layout.js.984db11e.js",
   e + "/_app/immutable/entry/_page.svelte.3717b407.js"
@@ -15,7 +15,7 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l
   e + "/favicon.png"
 ], r = [
   e + "/"
-], u = "1680352392644", c = `cache-${u}`, i = [
+], u = "1680352413356", n = `cache-${u}`, i = [
   ...l,
   // the app itself
   ...p,
@@ -25,14 +25,14 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l
 ];
 self.addEventListener("install", (a) => {
   async function t() {
-    await (await caches.open(c)).addAll(i);
+    await (await caches.open(n)).addAll(i);
   }
   a.waitUntil(t());
 });
 self.addEventListener("activate", (a) => {
   async function t() {
     for (const s of await caches.keys())
-      s !== c && await caches.delete(s);
+      s !== n && await caches.delete(s);
   }
   a.waitUntil(t());
 });
@@ -40,13 +40,13 @@ self.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET")
     return;
   async function t() {
-    const s = new URL(a.request.url), n = await caches.open(c);
+    const s = new URL(a.request.url), c = await caches.open(n);
     if (i.includes(s.pathname))
-      return n.match(a.request);
+      return c.match(a.request);
     try {
       return await fetch(a.request);
     } catch {
-      return n.match(a.request);
+      return c.match(a.request);
     }
   }
   a.respondWith(t());
