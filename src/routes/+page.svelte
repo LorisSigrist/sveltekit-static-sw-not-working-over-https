ba998e1029817df19017451417ec3ce42023acdb
Loris Sigrist <sigrist@hasch.io>
2023-04-01 14:30:09 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let request_status = 'not requested';
+
+    function make_request() {
+        request_status = 'pending';
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+            .then(()=>request_status = 'fulfilled')
+            .catch(()=>request_status = 'failed');
+        
+    }
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={make_request}>Make request</button>
+<p>Request status: {request_status}</p>
